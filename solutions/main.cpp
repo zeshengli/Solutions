27590b9db5a042d34d375e6a3087155a03965ab6
stephenrawson <rawson.stephen@gmail.com>
2016-03-02 22:04:01 -0800

    Initial Commit

---
+++ solutions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  solutions
+//
+//  Created by Stephen Rawson on 3/2/16.
+//  Copyright © 2016 Stephen Rawson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
